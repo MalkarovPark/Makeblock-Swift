a272388740c61d66f839097e2738f08e7900a838
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2023-04-27 15:54:48 +0300

    Updated the demo project.

--- DemoProject/DemoProject/ViewController.swift
+++ DemoProject/DemoProject/ViewController.swift
@@ -14,7 +14,7 @@ var mBot = MBot(connection: connection)
 
 class BluetoothDeviceTableViewCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
-    
+
 }
 
 class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
@@ -34,7 +34,7 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         }
         
         connection.onConnect = {
-            self.performSegueWithIdentifier("showDetails", sender: nil)
+            self.performSegue(withIdentifier: "showDetails", sender: nil)
         }
     }
 
@@ -43,16 +43,16 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         // Dispose of any resources that can be recreated.
     }
 
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return deviceList.count
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let deviceCell = deviceTableView.dequeueReusableCellWithIdentifier("deviceTableView", forIndexPath: indexPath) as! BluetoothDeviceTableViewCell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let deviceCell = deviceTableView.dequeueReusableCell(withIdentifier: "deviceTableView", for: indexPath) as! BluetoothDeviceTableViewCell
         let device = deviceList[indexPath.row]
         deviceCell.nameLabel.text = "\(device.name) (\(device.distance))"
         return deviceCell
@@ -60,40 +60,9 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         
     }
     
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let device = deviceList[indexPath.row]
-        connection.connect(device)
-    }
-}
-
-class DetailViewController: UIViewController {
-    @IBOutlet weak var ultrasonicValue: UILabel!
-    
-    @IBAction func onDisconnect(sender: AnyObject) {
-        connection.disconnect()
-        dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    @IBAction func onMoveForward(sender: AnyObject) {
-        mBot.moveForward(255)
-    }
-    
-    @IBAction func onStopMoving(sender: AnyObject) {
-        mBot.stopMoving()
-    }
-    
-    @IBAction func onRGBLED(sender: AnyObject) {
-        mBot.setRGBLED(.all, red: 255, green: 0, blue: 0)
-    }
-    
-    @IBAction func onBeepBuzzer(sender: AnyObject) {
-        mBot.setBuzzer(.C4, duration: .half)
-    }
-    
-    @IBAction func onUltrasonic(sender: AnyObject) {
-        mBot.getUltrasonicSensorValue() { value in
-            self.ultrasonicValue.text = "\(value.floatValue)"
-        }
+        connection.connect(device: device)
     }
 }
 
